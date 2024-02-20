507ff80b15e4e0935ca66508e862be055a436f4d
Ayush <ayush.6678@gmail.com>
2024-02-21 00:51:48 +0530

    new

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 
 // const Router = require("./routes")
+require("dotenv").config();
+const passport=require("passport");
+const cookieSession=require("cookie-session");
 const express = require('express');
 const cors = require('cors');
+const passportSetup=require("./passport");
+const authRoute=require("./routes/auth");
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(
+    cookieSession({
+        name:"session",
+        keys: ["harmony"],
+        maxAge:24*60*60*100,
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(cors({
+//     origin:"https://localhost:3000",
+//     methods:"GET,POST,PUT,DELETE",
+//     credentials:true,
+// }))
+app.use("/auth",authRoute);
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
